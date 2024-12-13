146a74b781d469827ea25d1c1ead0aca33e4372f
data_and_design <guerra.scarlet87@gmail.com>
2024-12-13 13:09:16 -0300

    lista la funcionalidad de VER

--- app_profes/urls.py
+++ app_profes/urls.py
@@ -4,9 +4,9 @@ from app_profes import views
 app_name = 'app_profes'
 
 urlpatterns = [
-    path('profes/', views.VerProfes.as_view(), name = 'profes_buscar'),
-    path('profes/crear', views.CrearProfe.as_view(), name = 'profe_nuevo')
-    
+    path('profes/', views.ListaProfes.as_view(), name = 'profes_buscar'),
+    path('profes/crear', views.CrearProfe.as_view(), name = 'profe_nuevo'),
+    path('profes/info/<int:pk>', views.InfoProfe.as_view(), name = 'profe_info')
     
     
     
--- app_profes/views.py
+++ app_profes/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from app_profes.models import Profes
 from django.urls import reverse_lazy
 
 
 # Visualizacion de la database de profesores de COOLates
-class VerProfes(ListView):
+class ListaProfes(ListView):
     model = Profes
     template_name = "app_profes/profes_buscar.html"
     
@@ -18,6 +19,12 @@ class CrearProfe(CreateView):
     model = Profes  # eso viene del model, entonces se trae igual
     template_name = "app_profes/profe_nuevo.html"
     fields = ['nombre', 'apellido', 'certificacion', 'horas_semana', 'fecha_ingreso']   # los campos de mi formulario
-    success_url = reverse_lazy('app_profes:profe_nuevo')
+    success_url = reverse_lazy('app_profes:profes_buscar')
     
-
+# Info de profes (Ver)
+class InfoProfe(DetailView):
+    model = Profes
+    template_name = "app_profes/profe_info.html"
+    # context_object_name = 'profe' no es necesario poner esto porque internamente 
+    # django genera el contect_object_name como el nombre del modelo pero en minuscula
+  
